433e10841b3c2892b123aecf93456c4981d6c08c
JoaoNunoAbreu <joaonunocgabreu@gmail.com>
2022-05-20 21:56:12 +0100

    update

--- src/propythia/DNA/deep_ml/encoding.py
+++ src/propythia/DNA/deep_ml/encoding.py
@@ -19,24 +19,59 @@ class DNAEncoding:
         if(checker(dna_sequence)):
             self.dna_sequence = dna_sequence.strip().upper()
 
-    def __init__(self, df: pd.DataFrame):
+    def __init__(self, column: np.ndarray):
+        arr = column.tolist()
         
-        if(df.columns.tolist() != ['sequence', 'label']):
-            print("Error! The dataframe must have two columns only: sequence and label.")
+        if(all(x == 1 or x == 0 for x in arr)):
+            self.labels = column
+            self.sequences = None
+        elif(all(checker(x) for x in arr)):
+            self.sequences = column
+            self.labels = None
+        else:
+            print("Error! All labels must be either 0 or 1.")
             sys.exit(1)
-        
-        for i in df['sequence']:
-            if(checker(i) == False):
-                print("Error! Invalid character in sequence.")
-                sys.exit(1)
-                
-        self.df = df
 
-    def one_hot_encode(self, enconde_target: bool = False):
+    # def __init__(self, df: pd.DataFrame):
+    #     if(df.columns.tolist() != ['sequence', 'label']):
+    #         print("Error! The dataframe must have two columns only: sequence and label.")
+    #         sys.exit(1)
+        
+    #     for i in df['sequence']:
+    #         if(checker(i) == False):
+    #             print("Error! Invalid character in sequence.")
+    #             sys.exit(1)
+                
+    #     self.df = df
+
+    # def one_hot_encode(self, enconde_target: bool = False):
+    #     """
+    #     From: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8138820/
+    #     Calculates binary encoding. Each nucleotide is encoded by a four digit binary vector.
+    #     :param enconde_target: If True, the target column will be encoded.
+    #     :return: list with values of binary encoding
+    #     """
+    #     binary = {
+    #         'A': [1, 0, 0, 0],
+    #         'C': [0, 1, 0, 0],
+    #         'G': [0, 0, 1, 0],
+    #         'T': [0, 0, 0, 1],
+    #         1: [1, 0],
+    #         0: [0, 1]
+    #     }
+
+    #     if(self.df is not None):
+    #         self.df['sequence'] = self.df['sequence'].apply(lambda x: np.array([binary[i] for i in x]))
+    #         if(enconde_target):
+    #             self.df['label'] = self.df['label'].apply(lambda x: np.array(binary[x]))
+    #         return self.df
+    #     else:
+    #         return [binary[i] for i in self.dna_sequence]
+    
+    def one_hot_encode(self):
         """
         From: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8138820/
         Calculates binary encoding. Each nucleotide is encoded by a four digit binary vector.
-        :param enconde_target: If True, the target column will be encoded.
         :return: list with values of binary encoding
         """
         binary = {
@@ -45,14 +80,12 @@ class DNAEncoding:
             'G': [0, 0, 1, 0],
             'T': [0, 0, 0, 1],
             1: [1, 0],
-            0: [0, 1]
+            0: [0, 1]            
         }
-
-        if(self.df is not None):
-            self.df['sequence'] = self.df['sequence'].apply(lambda x: np.array([binary[i] for i in x]))
-            if(enconde_target):
-                self.df['label'] = self.df['label'].apply(lambda x: np.array(binary[x]))
-            return self.df
+        if(self.labels is not None):
+            return np.array([binary[i] for i in self.labels])
+        elif(self.sequences is not None):
+            return np.array([[binary[i] for i in x] for x in self.sequences])
         else:
-            return [binary[i] for i in self.dna_sequence]
-        
+            print("Error! No labels or sequences were provided.")
+            sys.exit(1)
