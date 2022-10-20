c409faf6405b39a8ea383999fa9b99ccff382579
JoaoNunoAbreu <joaonunocgabreu@gmail.com>
2022-10-20 11:19:40 +0100

    Revert "smote in training"
    This reverts commit 477b27a05296ddec9521bf06fcb704c1a744724c.

--- src/propythia/DNA/src/prepare_data.py
+++ src/propythia/DNA/src/prepare_data.py
@@ -34,11 +34,6 @@ def data_splitting(fps_x, fps_y, batch_size, train_size, test_size, validation_s
         stratify=y
     )
     
-    print("Set shapes:")
-    print("x_train, y_train:", x_train.shape, y_train.shape)
-    print("x_cv, y_cv:", x_cv.shape, y_cv.shape)
-    print("x_test, y_test:", x_test.shape, y_test.shape)
-
     train_data = data_utils.TensorDataset(
         torch.tensor(x_train, dtype=torch.float),
         torch.tensor(y_train, dtype=torch.long)
@@ -120,7 +115,7 @@ def prepare_data(data_dir, mode, batch_size, k, dataset_file_format, cutting_len
             raise ValueError("dataset_file_format must be either 'csv' or 'fasta'.")
         
         # check if the sequences are of equal length
-        if len(set(dataset["sequence"].apply(lambda x: len(x)))) != 1 and mode != 'descriptor':
+        if len(set(dataset["sequence"].apply(lambda x: len(x)))) != 1:
             print("Sequences are not of equal length. Equalizing sequences length...")
             dataset = equalize_sequences_length(dataset, cutting_length)
         
@@ -129,6 +124,11 @@ def prepare_data(data_dir, mode, batch_size, k, dataset_file_format, cutting_len
             if mode == 'descriptor' \
             else calculate_encoders(dataset, mode, k) 
             
+        # if necessary, oversample data using SMOTE
+        # if any class has 60% or more of the data, oversampling is needed
+        if len(fps_y) * 0.6 < max([sum(fps_y == 0), sum(fps_y == 1)]):
+            fps_x, fps_y = oversample(fps_x, fps_y)
+        
         if save_to_pickle:
             save_pickle(fps_x_file, fps_y_file, fps_x, fps_y)
         
